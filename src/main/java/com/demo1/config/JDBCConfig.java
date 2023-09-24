9e26ec13176d0a7b719f3c2b7298574946eed762
zurichscud <zurichscud@outlook.com>
2023-09-24 14:20:48 +0800

    add transfer function

--- src/main/java/com/demo1/config/JDBCConfig.java
+++ src/main/java/com/demo1/config/JDBCConfig.java
@@ -3,6 +3,8 @@ package com.demo1.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -24,4 +26,11 @@ public class JDBCConfig {
         ds.setPassword(password);
         return ds;
     }
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
+    }
+
 }
--- src/main/java/com/demo1/config/SpringConfig.java
+++ src/main/java/com/demo1/config/SpringConfig.java
@@ -1,11 +1,13 @@
 package com.demo1.config;
 
 import org.springframework.context.annotation.*;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan("com.demo1")
 @PropertySource("classpath:jdbc.properties")
 @EnableAspectJAutoProxy
 @Import({MyBatisConfig.class, JDBCConfig.class})
+@EnableTransactionManagement
 public class SpringConfig {
 }
